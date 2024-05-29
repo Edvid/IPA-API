dff48b1a37dea0f8eb70d29012781c4c0ed4e539
Edvid <spacemastr@gmail.com>
2024-05-29 22:46:36 +0200

    Abandon jq keys strategy in favour of awk
    Now shows both key and value in columns inside fzf, for quick
    reference of what you are about to select. Uses awk to pick out
    only the character we need.

--- ipa.sh
+++ ipa.sh
@@ -1,18 +1,18 @@
-cat main.json | \
-jq -r ".letters[\"$(
-  cat main.json | \
-  jq -r ".letters | keys" | \
-  head -n -1 | tail -n +2 | \
-  sed 's/^ *//;s/\",//g;s/\"//g' | \
-  (
-    if [[ -n $1 ]]; then
-      fzf --filter="$1" | head -n 1
-    else
-      fzf
-    fi
-  )
-)\"]"
+#!/usr/bin/bash
 
+cat main.json | \
+jq -r ".letters" | \
+head -n -1 | tail -n +2 | \
+sed 's/^ *//;s/\",//g;s/\"//g' | \
+column -ts ':' | \
+(
+  if [[ -n $1 ]]; then
+    fzf --filter="$1" | head -n 1
+  else
+    fzf
+  fi
+) | \
+awk '{print $2}'
 
 
 
